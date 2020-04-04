2c17cf7a371f5d1cf8b829492c99bad5d63b6c77
supplient <437566830@qq.com>
2020-04-04 16:43:53 +0800

    停用掉304，为了方便调试

--- local_server.py
+++ local_server.py
@@ -2,6 +2,8 @@ from http.server import SimpleHTTPRequestHandler
 import socketserver
 import argparse
 import os
+from http import HTTPStatus
+import urllib
 
 ####################
 # 浏览器解析html文件中的"../lib/view.js"这类地址时，是从url出发的。
@@ -14,6 +16,49 @@ class MyHandler(SimpleHTTPRequestHandler):
     directory = None
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=MyHandler.directory, **kwargs)
+
+    def send_head(self):
+        """Override for do not respond 304(which use browser cache)
+        """
+        path = self.translate_path(self.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self.path)
+            if not parts.path.endswith('/'):
+                # redirect browser - doing basically what apache does
+                self.send_response(HTTPStatus.MOVED_PERMANENTLY)
+                new_parts = (parts[0], parts[1], parts[2] + '/',
+                             parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.send_header("Location", new_url)
+                self.end_headers()
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND, "File not found")
+            return None
+
+        try:
+            fs = os.fstat(f.fileno())
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", ctype)
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified",
+                self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
 
 def startServe(directory, address, port):
     HANDLER = MyHandler
